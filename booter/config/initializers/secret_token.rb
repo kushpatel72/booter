9afa90215fc01c2dfccc466c335ccafdde011a87
= <=>
2012-03-01 11:25:31 -0800

    First Commit Yay

---
+++ booter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Booter::Application.config.secret_token = '5aa8dff3b44b713ad2787d13af60ba2345f4e88906cc4ae37b8cd87473537d1b7953581accb73bcdf7e97bea132402b878188f0798c0e81c86d4b17cdd12dd55'
