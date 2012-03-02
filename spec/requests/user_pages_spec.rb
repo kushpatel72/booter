8a944670f15a0e4b14c2d6eb5ab35271af0f2b3b
= <=>
2012-03-02 15:07:29 -0800

    Finished Sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,26 +6,40 @@ describe "User pages" do
 
   describe "signup" do
 
-      before { visit signup_path }
+    before { visit signup_path }
 
-      describe "with invalid information" do
-        it "should not create a user" do
-          expect { click_button "Sign up" }.not_to change(User, :count)
-        end
-      end
-
-      describe "with valid information" do
-        before do
-          fill_in "Name",         with: "Example User"
-          fill_in "Email",        with: "user@example.com"
-          fill_in "Password",     with: "foobar"
-          fill_in "Confirmation", with: "foobar"
-        end
-
-        it "should create a user" do
-          expect { click_button "Sign up" }.to change(User, :count).by(1)
-        end
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button "Sign up" }.not_to change(User, :count)
       end
     end
-  
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button "Sign up" }.to change(User, :count).by(1)
+      end
+     
+      describe "after saving the user" do
+        before { click_button "Sign up" }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.flash.success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
+      
+      describe "followed by signout" do
+        before { click_button "Sign up" }
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
+    end
+  end
 end
